3aea82477aad3a4164d6caa5c05d65a7a3b60751
ccheung0926 <cindycheung0926@gmail.com>
2015-12-07 22:47:47 -0800

    [feat] working on rendering next month

--- public/index.js
+++ public/index.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
   var startOfMonth = moment().startOf("month")._d;
   var endOfMonth = moment().endOf("month")._d;
-  var firstDayOfMonth = startOfMonth.getDay();
-  var lastDateOfMonth = endOfMonth.getDate();
+  
   var currentMonth = moment().format("MMMM");
   var currentYear = moment().format("YYYY")
 
   var $month = $("#currentMonth");
   var $year = $("#currentYear");
-  var $theFirst = $("#"+firstDayOfMonth);
+  var $nextMonth = $("#nextMonth");
+  var $previousMonth = $("#previousMonth");
+  
 
   $month.append(currentMonth);
   $year.append(" "+currentYear);
-  $theFirst.append(1);
   var monthInNum = moment().format("MM");
   console.log(monthInNum)
   var start = moment([2016, 12]);
   console.log(moment(start).startOf('month'));
-  for(var i = 1; i < lastDateOfMonth; i++){
-    var id = firstDayOfMonth + i
-    $("#"+id).append(i+1);
-  }
-})
+
+  $nextMonth.on("click", function(e){
+    
+  });
+
+  (function calendar(firstDay, lastDay){
+    var firstDayOfMonth = firstDay.getDay();
+    var lastDateOfMonth = lastDay.getDate();
+    var $theFirst = $("#"+firstDayOfMonth);
+    $theFirst.append(1);
+    for(var i = 1; i < lastDateOfMonth; i++){
+      var id = firstDayOfMonth + i
+      $("#"+id).append(i+1);
+    }
+  })(startOfMonth, endOfMonth);
+});
