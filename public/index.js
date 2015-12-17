faf906c1c16899496edbda18541000f826b97d48
ccheung0926 <cindycheung0926@gmail.com>
2015-12-17 00:55:50 -0800

    next month

--- public/index.js
+++ public/index.js
@@ -1,35 +1,51 @@
 $(document).ready(function(){
+  var monthInNum = parseInt(moment().format("MM") - 1);
+  var currentYear = parseInt(moment().format("YYYY"));
   var startOfMonth = moment().startOf("month")._d;
-  var endOfMonth = moment().endOf("month")._d;
+  // var endOfMonth = moment().endOf("month")._d;
+  var endOfMonth = moment(currentYear+"-"+ monthInNum, "YYYY-MM").daysInMonth();
   
   var currentMonth = moment().format("MMMM");
-  var currentYear = moment().format("YYYY")
 
   var $month = $("#currentMonth");
   var $year = $("#currentYear");
   var $nextMonth = $("#nextMonth");
   var $previousMonth = $("#previousMonth");
   
-
   $month.append(currentMonth);
   $year.append(" "+currentYear);
-  var monthInNum = moment().format("MM");
-  console.log(monthInNum)
-  var start = moment([2016, 12]);
-  console.log(moment(start).startOf('month'));
+  console.log("monthInNum", monthInNum);
+  var start = moment([2016, 0]);
+  console.log(moment(start).startOf('month'), "heyy");
 
   $nextMonth.on("click", function(e){
-    
+    if(monthInNum + 1 >= 12){
+      currentYear++;
+      monthInNum = 0;
+      var firstDay = moment([currentYear, monthInNum])._d;
+      var lastDay = moment(currentYear+"-"+1, "YYYY-MM").daysInMonth();
+      $(".dateContent").empty();
+      calendar(firstDay, lastDay);
+      // console.log(date.getDay());
+    }
   });
 
-  (function calendar(firstDay, lastDay){
+  function calendar(firstDay, lastDay){
     var firstDayOfMonth = firstDay.getDay();
-    var lastDateOfMonth = lastDay.getDate();
+    // var lastDateOfMonth = lastDay.getDate();
     var $theFirst = $("#"+firstDayOfMonth);
+    var today = moment().format("Do").slice(0,-2);
     $theFirst.append(1);
-    for(var i = 1; i < lastDateOfMonth; i++){
+    for(var i = 1; i < lastDay; i++){
       var id = firstDayOfMonth + i
-      $("#"+id).append(i+1);
+      var date = i+1;
+      if(date == today){
+        $("#"+id).addClass("today").append(date);
+      }
+      else{
+        $("#"+id).append(date);
+      }
     }
-  })(startOfMonth, endOfMonth);
+  }
+  calendar(startOfMonth, endOfMonth);
 });
